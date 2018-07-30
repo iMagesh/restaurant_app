99d5813d3069961d9153ed8f15eeb2ee5aa7cbc3
Magesh <magesh@hash14.com>
2018-07-30 18:58:11 +0530

    Guest model spec updated

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
   it { should have_many(:reservations) }
+
+  it "is not valid without a name" do
+    guest = Guest.new(email: 'nl@example.com')
+    expect(guest).to_not be_valid
+  end
+
+  it "is not valid without a email" do
+    guest = Guest.new(name: 'myname')
+    expect(guest).to_not be_valid
+  end
+
+  it "is not valid without a correct email" do
+    guest = Guest.new(name: 'myname', email: 'myname.com')
+    expect(guest).to_not be_valid
+  end
+
 end
