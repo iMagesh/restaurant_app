3b066e88bd3e032f691b3ffd232a6c17f6e64e16
Magesh <magesh@hash14.com>
2018-07-30 19:21:15 +0530

    added validation for restaurant table and updated spec

--- app/models/restaurant_table.rb
+++ app/models/restaurant_table.rb
@@ -1,4 +1,16 @@
 class RestaurantTable < ActiveRecord::Base
   belongs_to :restaurant
   has_many :reservations
+
+  validates :name, presence: true
+  validates :min_guests, presence: true, numericality: true
+  validates :max_guests, presence: true, numericality: true
+  validate :min_max_guest
+
+  def min_max_guest
+    if (self.min_guests && self.max_guests) && (self.min_guests > self.max_guests)
+      errors.add(:restaurant_table, "min_guests count cannot be greater than max_guests")
+    end
+  end
+
 end
--- spec/models/restaurant_table_spec.rb
+++ spec/models/restaurant_table_spec.rb
@@ -3,4 +3,35 @@ require 'rails_helper'
 RSpec.describe RestaurantTable, type: :model do
   it { should have_many(:reservations) }
   it { should belong_to(:restaurant) }
+
+  it "is not valid without a name" do
+    restaurant_table = RestaurantTable.new(min_guests: 2, max_guests: 10)
+    expect(restaurant_table).to_not be_valid
+  end
+
+  it "is not valid without min guests count" do
+    restaurant_table = RestaurantTable.new(name: 'myname', max_guests: 10)
+    expect(restaurant_table).to_not be_valid
+  end
+
+  it "is not valid without max guests count" do
+    restaurant_table = RestaurantTable.new(name: 'myname', min_guests: 2)
+    expect(restaurant_table).to_not be_valid
+  end
+
+  it "is not valid when max guests is not a number" do
+    restaurant_table = RestaurantTable.new(name: 'myname', max_guests: "a1", min_guests: 2)
+    expect(restaurant_table).to_not be_valid
+  end
+
+  it "is not valid when min guests is not a number" do
+    restaurant_table = RestaurantTable.new(name: 'myname', min_guests: "a1", max_guests: 10)
+    expect(restaurant_table).to_not be_valid
+  end
+
+  it "is not valid when min_guests count is greater than max_guests count" do
+    restaurant_table = RestaurantTable.new(name: 'myname', min_guests: 10, max_guests: 3)
+    expect(restaurant_table).to_not be_valid
+  end
+
 end
