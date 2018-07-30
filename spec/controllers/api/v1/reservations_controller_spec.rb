7baa737c810882bf31bc202a5dfcd15ab23a18a9
Magesh <magesh@hash14.com>
2018-07-30 13:24:11 +0530

    reservation spec updated with list of reservation

--- spec/controllers/api/v1/reservations_controller_spec.rb
+++ spec/controllers/api/v1/reservations_controller_spec.rb
@@ -16,8 +16,8 @@ describe Api::V1::ReservationsController do
           restaurant_shift_id: RestaurantShift.last.id,
           guest_email: "something@gmail.com",
           guest_name: "Something",
-          reservation_from: "9am",
-          reservation_to: "10am"
+          reservation_from: "11am",
+          reservation_to: "12pm"
         }
       }
 
@@ -44,8 +44,8 @@ describe Api::V1::ReservationsController do
     it 'it should contain the correct reservation time' do
       from_time = json['reservation_from'].to_datetime.strftime("%l%P")
       to_time = json['reservation_to'].to_datetime.strftime("%l%P")
-      expect(from_time).to match("9am")
-      expect(to_time).to match("10am")
+      expect(from_time).to match("11am")
+      expect(to_time).to match("12pm")
     end
 
   end
@@ -98,60 +98,67 @@ describe Api::V1::ReservationsController do
   end
 
 
-  # context "when name or email is missing" do
-  #   let(:params) do
-  #     {
-  #       restaurant: {
-  #         name: "Magesh"
+  # context 'Update Reservation details' do
+
+
+  #   before(:example) do
+  #     FactoryGirl.create(:restaurant_with_tables_and_shifts)
+  #     res = FactoryGirl.create(:reservation, params)
+
+  #     params = {
+  #       id: res.id,
+  #       reservation: {
+  #         restaurant_table_id: RestaurantTable.last.id,
+  #         guests_count: 4,
+  #         restaurant_shift_id: RestaurantShift.last.id,
+  #         guest_email: "something@gmail.com",
+  #         guest_name: "Something",
+  #         reservation_from: "9am",
+  #         reservation_to: "10am"
   #       }
   #     }
+
+  #     patch :update, params, format: 'json'
   #   end
 
-  #   it "should render error messages for missing email" do
-  #     post :create, params
-  #     expect_forbidden_status
-  #     expect_json_content
-  #     expect(json["error"]).to eq(true)
-  #     expect(json["errors"]).to include(
-  #       "Email can't be blank")
+  #   it 'responds with a 200 status code' do
+  #     is_expected.to respond_with 200
   #   end
 
-  #   it "should render error messages for missing name and phone" do
-  #     post :create, {restaurant: {email: "sales@orangewok.com"}}
-  #     expect_forbidden_status
-  #     expect_json_content
-  #     expect(json["error"]).to eq(true)
-  #     expect(json["errors"]).to include(
-  #       "Name can't be blank", "Phone can't be blank")
+  #   it 'returns json' do
+  #     expect(response.content_type).to eq "application/json"
   #   end
 
-  #   it "should render error messages for invalid email" do
-  #     post :create, {restaurant: {email: "testmagesh.com"}}
-  #     expect_forbidden_status
-  #     expect_json_content
-  #     expect(json["error"]).to eq(true)
-  #     expect(json["errors"]).to include(
-  #       "Email is invalid")
+  #   it 'it shoud contain the reservation data' do
+  #     expect(json.keys).to include('id', 'restaurant_table_id', 'guests_count', 'restaurant_shift_id', 'guest_id', 'reservation_from', 'reservation_to')
+  #     expect(json['restaurant_table_id']).to match(@res.restaurant_tables.last.id)
+  #     expect(json['restaurant_shift_id']).to match(@res.restaurant_shifts.last.id)
+  #     expect(json['guests_count']).to match(4)
+  #     email = Guest.find(json['guest_id']).email
+  #     expect(email).to match("something@gmail.com")
   #   end
 
-  #   it "should render error messages for invalid phone with alphabet" do
-  #     post :create, {restaurant: {email: "something@gmail.com", phone: "A919952031234"}}
-  #     expect_forbidden_status
-  #     expect_json_content
-  #     expect(json["error"]).to eq(true)
-  #     expect(json["errors"]).to include(
-  #       "Phone is not a number")
-  #   end
-
-  #   it "should render error messages for wrong phone number format" do
-  #     post :create, {restaurant: {email: "something@gmail.com", phone: "00066919952031234"}}
-  #     expect_forbidden_status
-  #     expect_json_content
-  #     expect(json["error"]).to eq(true)
-  #     expect(json["errors"]).to include(
-  #       "Phone is too long")
+  #   it 'it should contain the correct reservation time' do
+  #     from_time = json['reservation_from'].to_datetime.strftime("%l%P")
+  #     to_time = json['reservation_to'].to_datetime.strftime("%l%P")
+  #     expect(from_time).to match("9am")
+  #     expect(to_time).to match("10am")
   #   end
 
   # end
+
+  context "List reservations for a given restaurant" do
+
+    before(:example) do
+      r = FactoryGirl.create(:reservation)
+      get :index, id: r.restaurant_table.restaurant.id, format: 'json'
+    end
+
+    it "should contain a array of reservations data" do
+      expect(json).not_to be_empty
+    end
+  end
+
+
 
 end
