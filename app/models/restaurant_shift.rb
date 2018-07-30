516d2e326a373241d473f717c457144605b756ea
Magesh <magesh@hash14.com>
2018-07-30 17:49:37 +0530

    Validation added for restaurant shift start and end time

--- app/models/restaurant_shift.rb
+++ app/models/restaurant_shift.rb
@@ -1,8 +1,14 @@
 class RestaurantShift < ActiveRecord::Base
   belongs_to :restaurant
 
+  validate :shift_start_end_time
+
   def shift_time
     "#{self.start_time.strftime("%l%P")} - #{self.end_time.strftime("%l%P")}"
+  end
+
+  def shift_start_end_time
+    errors.add(:restaurant_shift, "end time cannot be less than start time") if self.start_time > self.end_time
   end
 
 end
